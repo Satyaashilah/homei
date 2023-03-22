791546e24b9956db9bcdac9269438d2929820a3a
Satyaashilah <satyaashilah@gmail.com>
2023-03-22 15:34:45 +0700

    Routes Problem

---
+++ layout/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+  <li><router-link :to="(name: 'Home')">Home</router-link></li>
+  <li><router-link :to="(name: 'Home')">Home</router-link></li>
+  </ul>
+</template>
+
+<script setup>
+import {RouterView, RouterLink} from 'vue-router'
+</script>
+
+<style scoped>
+</style>
